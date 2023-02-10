ef82682e2cb8f3c6d45100f919ce0b27b693ae73
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-02-10 12:30:11 +0530

    10-02-2023 12:30

--- app/src/main/java/com/ashu/timetable/MainActivity.java
+++ app/src/main/java/com/ashu/timetable/MainActivity.java
@@ -3,13 +3,22 @@ package com.ashu.timetable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.Toolbar;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
-    private ListView ListView;
+    BottomNavigationView bottomNavigationView;
+
+    CoveredFragment coveredFragment = new CoveredFragment();
+    ThisWeekFragment thisWeekFragment = new ThisWeekFragment();
+    todayFragment todayFragment = new todayFragment();
+    fragment_weekend weekendFragment = new fragment_weekend();
+
 
 
 
@@ -17,5 +26,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        bottomNavigationView = findViewById(R.id.navigation_bar);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, todayFragment).commit();
+
+
+
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch(item.getItemId()) {
+                    case R.id.today:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, todayFragment).commit();
+                        return true;
+                    case R.id.thisWeek:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, thisWeekFragment).commit();
+                        return true;
+                    case R.id.covered:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, coveredFragment).commit();
+                        return true;
+                    case R.id.weekend:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, weekendFragment).commit();
+                        return true;
+
+                }
+
+                return false;
+            }
+        });
+
+
+
+
+
+
     }
 }
