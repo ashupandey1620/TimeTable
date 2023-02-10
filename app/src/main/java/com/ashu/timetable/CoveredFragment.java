473174804a69fea2ab87c9d8bd2c6b9cfbbed98b
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-02-10 12:11:19 +0530

    10-02-2023 12:11

---
+++ app/src/main/java/com/ashu/timetable/CoveredFragment.java
@@ -0,0 +1,24 @@
+package com.ashu.timetable;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class CoveredFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_covered, container, false);
+
+
+
+        return v;
+    }
+}
---
+++ app/src/main/java/com/ashu/timetable/ThisWeekFragment.java
@@ -0,0 +1,20 @@
+package com.ashu.timetable;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ThisWeekFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_this_week, container, false);
+    }
+}
