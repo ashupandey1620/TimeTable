f0eb1f716d8dda0f90b6bb96889b4d2f413c5491
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-06-27 13:07:32 +0530

    27-06-2023 Chart Support

--- app/src/main/java/com/ashu/timetable/ProfileFragment.java
+++ app/src/main/java/com/ashu/timetable/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.ashu.timetable;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,8 +9,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.CombinedChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.CombinedData;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class ProfileFragment extends Fragment {
+
+    private CombinedChart chart;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -17,9 +32,88 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        chart = v.findViewById(R.id.chart);
 
+        // Customize chart
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(true);
+
+        // Customize chart description
+        Description description = new Description();
+        description.setText("Line Chart Example");
+        chart.setDescription(description);
+
+        // Customize legend
+        Legend legend = chart.getLegend();
+        legend.setEnabled(true);
+
+        // Generate sample data
+        List<Entry> entries1 = generateData1();
+        List<Entry> entries2 = generateData2();
+        List<Entry> entries3 = generateData3();
+
+        // Create datasets and set colors
+        LineDataSet dataSet1 = new LineDataSet(entries1, "Data 1");
+        dataSet1.setColor(Color.RED);
+
+        LineDataSet dataSet2 = new LineDataSet(entries2, "Data 2");
+        dataSet2.setColor(Color.BLUE);
+
+        LineDataSet dataSet3 = new LineDataSet(entries3, "Data 3");
+        dataSet3.setColor(Color.GREEN);
+
+        // Create a list of LineDataSets
+        List<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(dataSet1);
+        dataSets.add(dataSet2);
+        dataSets.add(dataSet3);
+
+        // Create a LineData object and add the datasets
+        LineData lineData = new LineData(dataSets);
+
+        // Create a CombinedData object and add the LineData
+        CombinedData combinedData = new CombinedData();
+        combinedData.setData(lineData);
+
+        // Set the CombinedData to the chart
+        chart.setData(combinedData);
+        chart.invalidate();
 
 
         return v;
+    }
+    private List<Entry> generateData1() {
+        List<Entry> entries = new ArrayList<>();
+        // Add your data points here
+        entries.add(new Entry(0, 10));
+        entries.add(new Entry(1, 15));
+        entries.add(new Entry(2, 8));
+        entries.add(new Entry(3,7));
+        // Add more entries as needed
+        return entries;
+    }
+
+    // Example method to generate sample data for Data 2
+    private List<Entry> generateData2() {
+        List<Entry> entries = new ArrayList<>();
+        // Add your data points here
+        entries.add(new Entry(0, 5));
+        entries.add(new Entry(1, 12));
+        entries.add(new Entry(2, 3));
+        entries.add(new Entry(3,7));
+        // Add more entries as needed
+        return entries;
+    }
+
+    // Example method to generate sample data for Data 3
+    private List<Entry> generateData3() {
+        List<Entry> entries = new ArrayList<>();
+        // Add your data points here
+        entries.add(new Entry(0, 8));
+        entries.add(new Entry(1, 10));
+        entries.add(new Entry(2, 7));
+        entries.add(new Entry(3,7));
+        // Add more entries as needed
+        return entries;
     }
 }
